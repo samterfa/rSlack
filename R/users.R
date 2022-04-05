d6cdb4e75bdb5474e67206241880041c93a0b1fd
Sam Terfa <sam.terfa@reemahealth.com>
2022-04-05 17:56:26 -0500

    Update users.R

--- R/users.R
+++ R/users.R
@@ -26,3 +26,33 @@ users_list <- function(token = Sys.getenv("SLACK_TOKEN"), limit = 100, return_re
   
   body
 }
+
+
+#' Gets information about a user.
+#' 
+#' Gets information about a user.
+#' 
+#' @param token Authentication token bearing required scopes. Tokens should be passed as an HTTP Authorization header or alternatively, as a POST parameter.
+#' @param user User to get info on.
+#' @param include_locale Set this to TRUE to receive the locale for this user. Defaults to FALSE.
+#' @param return_response Whether or not to return the API call response as opposed to the response body. Defaults to FALSE (return response body)
+#' @return A list of channels
+#' @seealso \url{https://api.slack.com/methods/users.info}
+#' @family users
+#' @export
+users_info <- function(token = Sys.getenv("SLACK_TOKEN"), user, include_locale = NULL, return_response = F){
+  
+  query <- list(user = user, include_locale = include_locale)
+  
+  response <- httr::GET('https://slack.com/api/users.info', query = query,  httr::add_headers(Authorization = glue::glue('Bearer {token}')))
+  
+  if(return_response) return(response)
+  
+  body <- httr::content(response)
+  
+  if(!body$ok){
+    stop(body$error)
+  }
+  
+  body
+}
